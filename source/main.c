62137c9ccb613a42af91113901092f20579cc8bd
Joshua Sims <jm8hjkl@gmail.com>
2026-07-11 22:46:58 -0400

    kitten example

--- source/main.c
+++ source/main.c
@@ -1,30 +1,110 @@
 #include <3ds.h>
 #include <citro3d.h>
+#include <tex3ds.h>
 #include <string.h>
 #include "vshader_shbin.h"
+#include "kitten_t3x.h"
 
 #define CLEAR_COLOR 0x68B0D8FF
 
 #define DISPLAY_TRANSFER_FLAGS (GX_TRANSFER_FLIP_VERT(0) | GX_TRANSFER_OUT_TILED(0) | GX_TRANSFER_RAW_COPY(0) | GX_TRANSFER_IN_FORMAT(GX_TRANSFER_FMT_RGBA8) | GX_TRANSFER_OUT_FORMAT(GX_TRANSFER_FMT_RGB8) | GX_TRANSFER_SCALING(GX_TRANSFER_SCALE_NO))
 
 typedef struct {
-    float x, y, z;
+    float position[3];
+    float texcoord[2];
+    float normal[3];
 } vertex;
 
 static const vertex vertex_list[] = {
-    {200.0f, 200.0f, 0.5f},
-    {100.0f, 40.0f, 0.5f},
-    {300.0f, 40.0f, 0.5f},
+    // First face (PZ)
+    // First triangle
+    {{-0.5f, -0.5f, +0.5f}, {0.0f, 0.0f}, {0.0f, 0.0f, +1.0f}},
+    {{+0.5f, -0.5f, +0.5f}, {1.0f, 0.0f}, {0.0f, 0.0f, +1.0f}},
+    {{+0.5f, +0.5f, +0.5f}, {1.0f, 1.0f}, {0.0f, 0.0f, +1.0f}},
+    // Second triangle
+    {{+0.5f, +0.5f, +0.5f}, {1.0f, 1.0f}, {0.0f, 0.0f, +1.0f}},
+    {{-0.5f, +0.5f, +0.5f}, {0.0f, 1.0f}, {0.0f, 0.0f, +1.0f}},
+    {{-0.5f, -0.5f, +0.5f}, {0.0f, 0.0f}, {0.0f, 0.0f, +1.0f}},
+
+    // Second face (MZ)
+    // First triangle
+    {{-0.5f, -0.5f, -0.5f}, {0.0f, 0.0f}, {0.0f, 0.0f, -1.0f}},
+    {{-0.5f, +0.5f, -0.5f}, {1.0f, 0.0f}, {0.0f, 0.0f, -1.0f}},
+    {{+0.5f, +0.5f, -0.5f}, {1.0f, 1.0f}, {0.0f, 0.0f, -1.0f}},
+    // Second triangle
+    {{+0.5f, +0.5f, -0.5f}, {1.0f, 1.0f}, {0.0f, 0.0f, -1.0f}},
+    {{+0.5f, -0.5f, -0.5f}, {0.0f, 1.0f}, {0.0f, 0.0f, -1.0f}},
+    {{-0.5f, -0.5f, -0.5f}, {0.0f, 0.0f}, {0.0f, 0.0f, -1.0f}},
+
+    // Third face (PX)
+    // First triangle
+    {{+0.5f, -0.5f, -0.5f}, {0.0f, 0.0f}, {+1.0f, 0.0f, 0.0f}},
+    {{+0.5f, +0.5f, -0.5f}, {1.0f, 0.0f}, {+1.0f, 0.0f, 0.0f}},
+    {{+0.5f, +0.5f, +0.5f}, {1.0f, 1.0f}, {+1.0f, 0.0f, 0.0f}},
+    // Second triangle
+    {{+0.5f, +0.5f, +0.5f}, {1.0f, 1.0f}, {+1.0f, 0.0f, 0.0f}},
+    {{+0.5f, -0.5f, +0.5f}, {0.0f, 1.0f}, {+1.0f, 0.0f, 0.0f}},
+    {{+0.5f, -0.5f, -0.5f}, {0.0f, 0.0f}, {+1.0f, 0.0f, 0.0f}},
+
+    // Fourth face (MX)
+    // First triangle
+    {{-0.5f, -0.5f, -0.5f}, {0.0f, 0.0f}, {-1.0f, 0.0f, 0.0f}},
+    {{-0.5f, -0.5f, +0.5f}, {1.0f, 0.0f}, {-1.0f, 0.0f, 0.0f}},
+    {{-0.5f, +0.5f, +0.5f}, {1.0f, 1.0f}, {-1.0f, 0.0f, 0.0f}},
+    // Second triangle
+    {{-0.5f, +0.5f, +0.5f}, {1.0f, 1.0f}, {-1.0f, 0.0f, 0.0f}},
+    {{-0.5f, +0.5f, -0.5f}, {0.0f, 1.0f}, {-1.0f, 0.0f, 0.0f}},
+    {{-0.5f, -0.5f, -0.5f}, {0.0f, 0.0f}, {-1.0f, 0.0f, 0.0f}},
+
+    // Fifth face (PY)
+    // First triangle
+    {{-0.5f, +0.5f, -0.5f}, {0.0f, 0.0f}, {0.0f, +1.0f, 0.0f}},
+    {{-0.5f, +0.5f, +0.5f}, {1.0f, 0.0f}, {0.0f, +1.0f, 0.0f}},
+    {{+0.5f, +0.5f, +0.5f}, {1.0f, 1.0f}, {0.0f, +1.0f, 0.0f}},
+    // Second triangle
+    {{+0.5f, +0.5f, +0.5f}, {1.0f, 1.0f}, {0.0f, +1.0f, 0.0f}},
+    {{+0.5f, +0.5f, -0.5f}, {0.0f, 1.0f}, {0.0f, +1.0f, 0.0f}},
+    {{-0.5f, +0.5f, -0.5f}, {0.0f, 0.0f}, {0.0f, +1.0f, 0.0f}},
+
+    // Sixth face (MY)
+    // First triangle
+    {{-0.5f, -0.5f, -0.5f}, {0.0f, 0.0f}, {0.0f, -1.0f, 0.0f}},
+    {{+0.5f, -0.5f, -0.5f}, {1.0f, 0.0f}, {0.0f, -1.0f, 0.0f}},
+    {{+0.5f, -0.5f, +0.5f}, {1.0f, 1.0f}, {0.0f, -1.0f, 0.0f}},
+    // Second triangle
+    {{+0.5f, -0.5f, +0.5f}, {1.0f, 1.0f}, {0.0f, -1.0f, 0.0f}},
+    {{-0.5f, -0.5f, +0.5f}, {0.0f, 1.0f}, {0.0f, -1.0f, 0.0f}},
+    {{-0.5f, -0.5f, -0.5f}, {0.0f, 0.0f}, {0.0f, -1.0f, 0.0f}},
 };
 
 #define vertex_list_count (sizeof(vertex_list) / sizeof(vertex_list[0]))
 
 static DVLB_s *vshader_dvlb;
 static shaderProgram_s program;
-static int uLoc_projection;
+static int uLoc_projection, uLoc_modelView;
+static int uLoc_lightVec, uLoc_lightHalfVec, uLoc_lightClr, uLoc_material;
 static C3D_Mtx projection;
+static C3D_Mtx material = {{
+    {{0.0f, 0.2f, 0.2f, 0.2f}}, // Ambient
+    {{0.0f, 0.4f, 0.4f, 0.4f}}, // Diffuse
+    {{0.0f, 0.8f, 0.8f, 0.8f}}, // Specular
+    {{1.0f, 0.0f, 0.0f, 0.0f}}, // Emission
+}};
 
 static void *vbo_data;
+static C3D_Tex kitten_tex;
+static float angleX = 0.0, angleY = 0.0;
+
+// Helper function for loading a texture from memory
+static bool loadTextureFromMem(C3D_Tex *tex, C3D_TexCube *cube, const void *data, size_t size) {
+    Tex3DS_Texture t3x = Tex3DS_TextureImport(data, size, tex, cube, false);
+    if (!t3x)
+        return false;
+
+    // Delete the t3x object since we don't need it
+    Tex3DS_TextureFree(t3x);
+    return true;
+}
 
 static void sceneInit(void) {
     // Load the vertex shader, create a shader program and bind it
@@ -34,19 +114,22 @@ static void sceneInit(void) {
     C3D_BindProgram(&program);
 
     // Get the location of the uniforms
-    uLoc_projection = shaderInstanceGetUniformLocation(program.vertexShader, "projection");
+    uLoc_projection   = shaderInstanceGetUniformLocation(program.vertexShader, "projection");
+    uLoc_modelView    = shaderInstanceGetUniformLocation(program.vertexShader, "modelView");
+    uLoc_lightVec     = shaderInstanceGetUniformLocation(program.vertexShader, "lightVec");
+    uLoc_lightHalfVec = shaderInstanceGetUniformLocation(program.vertexShader, "lightHalfVec");
+    uLoc_lightClr     = shaderInstanceGetUniformLocation(program.vertexShader, "lightClr");
+    uLoc_material     = shaderInstanceGetUniformLocation(program.vertexShader, "material");
 
     // Configure attributes for use with the vertex shader
     C3D_AttrInfo *attrInfo = C3D_GetAttrInfo();
     AttrInfo_Init(attrInfo);
     AttrInfo_AddLoader(attrInfo, 0, GPU_FLOAT, 3); // v0=position
-    AttrInfo_AddFixed(attrInfo, 1);                // v1=color
-
-    // Set the fixed attribute (color) to solid white
-    C3D_FixedAttribSet(1, 1.0, 1.0, 1.0, 1.0);
+    AttrInfo_AddLoader(attrInfo, 1, GPU_FLOAT, 2); // v1=texcoord
+    AttrInfo_AddLoader(attrInfo, 2, GPU_FLOAT, 3); // v2=normal
 
     // Compute the projection matrix
-    Mtx_OrthoTilt(&projection, 0.0, 400.0, 0.0, 240.0, 0.0, 1.0, true);
+    Mtx_PerspTilt(&projection, C3D_AngleFromDegrees(80.0f), C3D_AspectRatioTop, 0.01f, 1000.0f, false);
 
     // Create the VBO (vertex buffer object)
     vbo_data = linearAlloc(sizeof(vertex_list));
@@ -55,25 +138,51 @@ static void sceneInit(void) {
     // Configure buffers
     C3D_BufInfo *bufInfo = C3D_GetBufInfo();
     BufInfo_Init(bufInfo);
-    BufInfo_Add(bufInfo, vbo_data, sizeof(vertex), 1, 0x0);
+    BufInfo_Add(bufInfo, vbo_data, sizeof(vertex), 3, 0x210);
 
-    // Configure the first fragment shading substage to just pass through the vertex color
+    // Load the texture and bind it to the first texture unit
+    if (!loadTextureFromMem(&kitten_tex, NULL, kitten_t3x, kitten_t3x_size))
+        svcBreak(USERBREAK_PANIC);
+    C3D_TexSetFilter(&kitten_tex, GPU_LINEAR, GPU_NEAREST);
+    C3D_TexBind(0, &kitten_tex);
+
+    // Configure the first fragment shading substage to blend the texture color with
+    // the vertex color (calculated by the vertex shader using a lighting algorithm)
     // See https://www.opengl.org/sdk/docs/man2/xhtml/glTexEnv.xml for more insight
     C3D_TexEnv *env = C3D_GetTexEnv(0);
     C3D_TexEnvInit(env);
-    C3D_TexEnvSrc(env, C3D_Both, GPU_PRIMARY_COLOR, 0, 0);
-    C3D_TexEnvFunc(env, C3D_Both, GPU_REPLACE);
+    C3D_TexEnvSrc(env, C3D_Both, GPU_TEXTURE0, GPU_PRIMARY_COLOR, 0);
+    C3D_TexEnvFunc(env, C3D_Both, GPU_MODULATE);
 }
 
 static void sceneRender(void) {
+    // Calculate the modelView matrix
+    C3D_Mtx modelView;
+    Mtx_Identity(&modelView);
+    Mtx_Translate(&modelView, 0.0, 0.0, -2.0 + 0.5 * sinf(angleX), true);
+    Mtx_RotateX(&modelView, angleX, true);
+    Mtx_RotateY(&modelView, angleY, true);
+
+    // Rotate the cube each frame
+    angleX += M_PI / 180;
+    angleY += M_PI / 360;
+
     // Update the uniforms
     C3D_FVUnifMtx4x4(GPU_VERTEX_SHADER, uLoc_projection, &projection);
+    C3D_FVUnifMtx4x4(GPU_VERTEX_SHADER, uLoc_modelView, &modelView);
+    C3D_FVUnifMtx4x4(GPU_VERTEX_SHADER, uLoc_material, &material);
+    C3D_FVUnifSet(GPU_VERTEX_SHADER, uLoc_lightVec, 0.0f, 0.0f, -1.0f, 0.0f);
+    C3D_FVUnifSet(GPU_VERTEX_SHADER, uLoc_lightHalfVec, 0.0f, 0.0f, -1.0f, 0.0f);
+    C3D_FVUnifSet(GPU_VERTEX_SHADER, uLoc_lightClr, 1.0f, 1.0f, 1.0f, 1.0f);
 
     // Draw the VBO
     C3D_DrawArrays(GPU_TRIANGLES, 0, vertex_list_count);
 }
 
 static void sceneExit(void) {
+    // Free the texture
+    C3D_TexDelete(&kitten_tex);
+
     // Free the VBO
     linearFree(vbo_data);
 
